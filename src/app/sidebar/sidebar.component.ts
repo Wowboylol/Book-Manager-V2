91717e34f807fcc402c6712fb1628196fcabe650
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2024-03-08 03:17:59 -0800

    Add: Sidebar menu items and style

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -2,17 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-sidebar',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+	selector: 'app-sidebar',
+	standalone: true,
+	imports: [CommonModule],
+	templateUrl: './sidebar.component.html',
+	styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit 
+{
+	close: boolean = true;
 
-  constructor() { }
+	constructor() { }
+	ngOnInit(): void { }
 
-  ngOnInit(): void {
-  }
-
+	toggleSidebar() { this.close = !this.close; }
 }
