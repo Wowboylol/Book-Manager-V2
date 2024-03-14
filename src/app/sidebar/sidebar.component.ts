3bc300b11ad29bcea32cf3024ba592ff8ae38fb9
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2024-03-13 22:38:52 -0700

    Update: project services directory

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ThemeService } from '../shared/theme.service';
+import { ThemeService } from '../shared/services/theme.service';
 
 @Component({
 	selector: 'app-sidebar',
