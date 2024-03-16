9e83665b8811d19fedf5c50167d182154b68edd2
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2024-03-16 01:25:10 -0700

    Update: Image lazy loading

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -3,5 +3,6 @@ import { BooksComponent } from './books/books.component';
 
 export const routes: Route[] = [
     { path: '', redirectTo: '/books', pathMatch: 'full' },
+    // { path: 'books', loadComponent: () => import('./books/books.component').then(mod => mod.BooksComponent) },
     { path: 'books', component: BooksComponent },
 ];
