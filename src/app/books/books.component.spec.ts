5cb37df86bde7e526308b4ed7089206124b62481
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2024-03-15 17:48:53 -0700

    Add: Basic book component unit tests

--- src/app/books/books.component.spec.ts
+++ src/app/books/books.component.spec.ts
@@ -3,21 +3,29 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BooksComponent } from './books.component';
 
 describe('BooksComponent', () => {
-  let component: BooksComponent;
-  let fixture: ComponentFixture<BooksComponent>;
+	let component: BooksComponent;
+	let fixture: ComponentFixture<BooksComponent>;
+	let bookItems: NodeListOf<Element>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ BooksComponent ]
-    })
-    .compileComponents();
+  	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [ BooksComponent ]
+		})
+		.compileComponents();
 
-    fixture = TestBed.createComponent(BooksComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(BooksComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+		// Get HTML elements
+		bookItems = fixture.nativeElement.querySelectorAll("app-book-item");
+	});
+
+	it('should create book component', () => {
+		expect(component).toBeTruthy();
+	});
+
+	it('should have same amount of book items as stored books', () => {
+		expect(bookItems.length).toBe(component.books.length);
+	});
 });
