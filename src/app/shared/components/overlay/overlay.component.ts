a049428fc566e1919a0e71d9ceed97dbb6513e9a
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2024-03-19 22:50:37 -0700

    Add: Overlay component template

--- src/app/shared/components/overlay/overlay.component.ts
+++ src/app/shared/components/overlay/overlay.component.ts
@@ -1,18 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-overlay',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './overlay.component.html',
-  styleUrls: ['./overlay.component.css']
+	selector: 'app-overlay',
+	standalone: true,
+	imports: [CommonModule],
+	templateUrl: './overlay.component.html',
+	styleUrls: ['./overlay.component.css']
 })
-export class OverlayComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class OverlayComponent
+{
+	constructor() { }
 }
