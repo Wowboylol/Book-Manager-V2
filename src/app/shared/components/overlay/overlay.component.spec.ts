d7ad21f18e0088d8c85c7b929771c48029a6552b
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2024-03-22 03:25:21 -0700

    Add: Overlay component unit tests

--- src/app/shared/components/overlay/overlay.component.spec.ts
+++ src/app/shared/components/overlay/overlay.component.spec.ts
@@ -5,21 +5,39 @@ import { OverlayComponent } from './overlay.component';
 import { routes } from 'src/app/app-routing';
 
 describe('OverlayComponent', () => {
-  let component: OverlayComponent;
-  let fixture: ComponentFixture<OverlayComponent>;
+	let component: OverlayComponent;
+	let fixture: ComponentFixture<OverlayComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ OverlayComponent, RouterTestingModule.withRoutes(routes) ]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [ OverlayComponent, RouterTestingModule.withRoutes(routes) ]
+		})
+    	.compileComponents();
 
-    fixture = TestBed.createComponent(OverlayComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(OverlayComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+  	});
 
-  it('should create overlay component', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create overlay component', () => {
+		expect(component).toBeTruthy();
+	});
+
+	it('should set overlay title based on displayed component', () => {
+		const displayedComponent = { overlayTitle: 'Test Title' };
+		component.updateOverlayTitle(displayedComponent);
+		expect(component.overlayTitle).toEqual('Test Title');
+	});
+
+	it('should set overflow hidden on body element on init', () => {
+		const rendererSpy = spyOn(component["renderer"], 'setStyle');
+		component.ngOnInit();
+		expect(rendererSpy).toHaveBeenCalledWith(component["document"].body, 'overflow', 'hidden');
+	});
+
+	it('should remove overflow hidden on body element on destroy', () => {
+		const rendererSpy = spyOn(component["renderer"], 'removeStyle');
+		component.ngOnDestroy();
+		expect(rendererSpy).toHaveBeenCalledWith(component["document"].body, 'overflow');
+	});
 });
