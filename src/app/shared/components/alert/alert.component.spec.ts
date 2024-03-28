225fabb0aacdf1f144c7fe04a670f9b7f1fa2675
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2024-03-28 02:46:07 -0700

    Add: Alert component unit tests

--- src/app/shared/components/alert/alert.component.spec.ts
+++ src/app/shared/components/alert/alert.component.spec.ts
@@ -5,6 +5,8 @@ import { AlertComponent } from './alert.component';
 describe('AlertComponent', () => {
 	let component: AlertComponent;
 	let fixture: ComponentFixture<AlertComponent>;
+	let alertElement: HTMLElement;
+	let messageElement: HTMLElement;
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
@@ -15,9 +17,28 @@ describe('AlertComponent', () => {
 		fixture = TestBed.createComponent(AlertComponent);
 		component = fixture.componentInstance;
 		fixture.detectChanges();
+
+		// Get HTML elements
+		alertElement = fixture.nativeElement.querySelector("#alert-box");
+		messageElement = fixture.nativeElement.querySelector("#alert-text");
 	});
 
 	it('should create alert component', () => {
 		expect(component).toBeTruthy();
+	});
+
+	it('should display message', () => {
+		component.message = 'Test message';
+		fixture.detectChanges();
+		expect(messageElement.textContent).toBe('Test message');
+	});
+
+	it('should be able to display all alert types', () => {
+		let alertTypes: string[] = ['primary', 'secondary', 'success', 'info', 'warning', 'danger', 'light', 'dark'];
+		alertTypes.forEach((type: string) => {
+			component.type = type;
+			fixture.detectChanges();
+			expect(alertElement.classList.contains(`alert-${type}`)).toBe(true);
+		});
 	});
 });
