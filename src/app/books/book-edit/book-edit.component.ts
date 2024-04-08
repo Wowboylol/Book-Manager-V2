f81a59d81536f07f13ae200edc863ffcd32bad7e
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2024-04-07 18:42:19 -0700

    Add: Book edit tags data sync

--- src/app/books/book-edit/book-edit.component.ts
+++ src/app/books/book-edit/book-edit.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Params } from '@angular/router';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 import { Book } from 'src/app/shared/models/book.model';
 import { BookService } from 'src/app/shared/services/book.service';
@@ -31,24 +31,38 @@ export class BookEditComponent implements OnInit
 	ngOnInit(): void { 
 		this.route.params.subscribe(
 			(params: Params) => {
-				let bookId = +params['id'];
-				this.book = this.bookService.getBookById(bookId);
 				this.editMode = params['id'] != null;
+				this.book = this.editMode ? this.bookService.getBookById(+params['id']) : null;
 				this.overlayResolve(this.editMode ? 'Edit Book' : 'Add Book');
 				this.initForm();
 			}
 		);
 	}
 
+	get tagControls(): FormArray {
+		return (<FormArray>this.bookForm.get('tags')).value;
+	}
+
 	private initForm(): void 
 	{
+		let tagFormArray = new FormArray([]);
+
+		if (this.editMode && this.book.tags) {
+			for (let tag of this.book.tags) {
+				tagFormArray.push(
+					new FormGroup({ 'name': new FormControl(tag) })
+				);
+			}
+		}
+
 		this.bookForm = new FormGroup({
 			'name': new FormControl(this.editMode ? this.book.name : null),
 			'imagePath': new FormControl(this.editMode ? this.book.imagePath : null),
 			'link': new FormControl(this.editMode ? this.book.link : null),
 			'rating': new FormControl(this.editMode ? this.book.rating : null),
 			'collection': new FormControl(this.editMode ? this.book.collection : null),
-			'description': new FormControl(this.editMode ? this.book.description : null)
+			'description': new FormControl(this.editMode ? this.book.description : null),
+			'tags': tagFormArray
 		});
 	}
 
