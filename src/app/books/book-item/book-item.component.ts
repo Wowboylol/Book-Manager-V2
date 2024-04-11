3117c61d7301132f5b7dfe0b743027da7da32c02
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2024-04-10 17:29:49 -0700

    Fix: Book item image gap javascript solution

--- src/app/books/book-item/book-item.component.ts
+++ src/app/books/book-item/book-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, Renderer2, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
@@ -15,16 +15,28 @@ import { TooltipDirective } from 'src/app/shared/directives/tooltip.directive';
 	templateUrl: './book-item.component.html',
 	styleUrls: ['./book-item.component.css']
 })
-export class BookItemComponent implements OnInit 
+export class BookItemComponent implements OnDestroy
 {
+	// Book item data
 	@Input() book: Book;
 	@Input() displayStyle: BookDisplayType
 	readonly displayEnum = BookDisplayType;
 	stars: number[] = [1, 2, 3, 4, 5];
 
-	constructor(private tagService: TagService) { }
+	// Dynamic styling data
+	@ViewChild('imageContainer') imageContainer: ElementRef;
+	@ViewChild('detailsContainer') detailsContainer: ElementRef;
+	resizeObserver: ResizeObserver = new ResizeObserver(entries => {
+		entries.forEach(entry => {
+			this.fillImageGap(entry.target.clientHeight);
+		});
+	});
 
-	ngOnInit(): void { }
+	constructor(private tagService: TagService, private renderer: Renderer2) { }
+
+	ngOnDestroy(): void {
+		this.resizeObserver.disconnect();
+	}
 
 	getTagAmount(name: string): number {
 		return this.tagService.getTagByName(name).amount;
@@ -32,5 +44,25 @@ export class BookItemComponent implements OnInit
 
 	getTagDescription(name: string): string {
 		return this.tagService.getTagByName(name).description;
+	}
+
+	// Run fillImageGap() and observes image height changes after image load
+	onImageLoad(event: Event) {
+		const imageElement = event.target as HTMLImageElement;
+		this.fillImageGap(imageElement.height);
+		this.resizeObserver.observe(imageElement);
+	}
+
+	// Fills gap between image and details container if image is smaller than details container (only for grid view)
+	private fillImageGap(imageElementHeight: number) {
+		if(this.displayStyle === BookDisplayType.List) {
+			this.renderer.setStyle(this.detailsContainer.nativeElement, 'margin-top', '0px');
+		}
+		else {
+			const imageGap = this.imageContainer.nativeElement.clientHeight - imageElementHeight;
+			if(imageGap > 0) {
+				this.renderer.setStyle(this.detailsContainer.nativeElement, 'margin-top', `-${imageGap}px`);
+			}
+		}
 	}
 }
