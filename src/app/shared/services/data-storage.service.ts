88b49604429a38a98b059cc053c3a9b3dd09e14c
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2024-04-14 04:57:49 -0700

    Update: Data storage service for collection data

--- src/app/shared/services/data-storage.service.ts
+++ src/app/shared/services/data-storage.service.ts
@@ -5,8 +5,10 @@ import { map } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
 import { BookService } from './book.service';
 import { TagService } from './tag.service';
+import { CollectionService } from './collection.service';
 import { Tag } from '../models/tag.model';
 import { Book } from '../models/book.model';
+import { Collection } from '../models/collection.model';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService 
@@ -14,14 +16,16 @@ export class DataStorageService
 	constructor(
 		private http: HttpClient, 
 		private bookService: BookService, 
-		private tagService: TagService
+		private tagService: TagService,
+		private collectionService: CollectionService
 	) { }
 
 	storeData() {
 		const books = this.bookService.getAllBooks();
 		const tags = this.tagService.getAllTags();
+		const collections = this.collectionService.getAllCollections();
 
-		this.http.put(`${environment.firebaseEndpoint}data.json`, { tags, books })
+		this.http.put(`${environment.firebaseEndpoint}data.json`, { tags, books, collections })
 			.subscribe(
 				response => {
 					console.log('Stored data:', response);
@@ -30,11 +34,12 @@ export class DataStorageService
 	}
 
 	fetchData() {
-		this.http.get<{ tags: Tag[], books: Book[] }>(`${environment.firebaseEndpoint}data.json`)
+		this.http.get<{ tags: Tag[], books: Book[], collections: Collection[] }>(`${environment.firebaseEndpoint}data.json`)
 			.pipe(
 				map(data => {
-					if(!data || !data.books) { return { tags: [], books: [] }; }
+					if(!data || !data.books) { return { tags: [], books: [], collections: [] }; }
 					if(!data.tags) { data.tags = []; }
+					if(!data.collections) { data.collections = []; }
 					return {
 						tags: data.tags.map(tag => {
 							return {...tag, description: tag.description ? tag.description : null};
@@ -46,7 +51,8 @@ export class DataStorageService
 								dateUpdated: book.dateUpdated ? new Date(book.dateUpdated) : null,
 								tags: book.tags ? book.tags : []
 							};
-						})
+						}),
+						collections: data.collections
 					}
 				})
 			)
@@ -55,6 +61,7 @@ export class DataStorageService
 					console.log('Fetched data:', data);
 					this.tagService.setTags(data['tags']);
 					this.bookService.setBooks(data['books']);
+					this.collectionService.setCollections(data['collections']);
 				}
 			);
 	}
