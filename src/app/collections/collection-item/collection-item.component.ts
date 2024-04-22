366b1368598a2183f5990a86eb484ac6455993e3
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2024-04-22 04:43:07 -0700

    Add: Collection item display

--- src/app/collections/collection-item/collection-item.component.ts
+++ src/app/collections/collection-item/collection-item.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { Collection } from 'src/app/shared/models/collection.model';
+import { BookService } from 'src/app/shared/services/book.service';
 
 @Component({
     selector: 'app-collection-item',
@@ -10,6 +13,18 @@ import { CommonModule } from '@angular/common';
 })
 export class CollectionItemComponent implements OnInit 
 {
-    constructor() { }
+    @Input() collection: Collection;
+
+    constructor(private bookService: BookService) { }
+
     ngOnInit(): void { }
+
+    getCollectionImage(): string {
+        let booksWithCollection = this.bookService.getBooksByCollection(this.collection.name);
+        return `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.8)), url(${booksWithCollection[0] ? booksWithCollection[0].imagePath : ''})`;
+    }
+
+    getCollectionColor(): string {
+        return this.collection.color;
+    }
 }
