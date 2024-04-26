fb98fff02988bd37447e69e3416406f8b7fda8e7
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2024-04-26 02:50:11 -0700

    Add: User-based data storage

--- src/app/shared/services/data-storage.service.ts
+++ src/app/shared/services/data-storage.service.ts
@@ -9,6 +9,7 @@ import { CollectionService } from './collection.service';
 import { Tag } from '../models/tag.model';
 import { Book } from '../models/book.model';
 import { Collection } from '../models/collection.model';
+import { AuthService } from './auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService 
@@ -17,7 +18,8 @@ export class DataStorageService
 		private http: HttpClient, 
 		private bookService: BookService, 
 		private tagService: TagService,
-		private collectionService: CollectionService
+		private collectionService: CollectionService, 
+		private authService: AuthService
 	) { }
 
 	storeData() {
@@ -25,44 +27,49 @@ export class DataStorageService
 		const tags = this.tagService.getAllTags();
 		const collections = this.collectionService.getAllCollections();
 
-		this.http.put(`${environment.firebaseEndpoint}data.json`, { tags, books, collections })
-			.subscribe(
-				response => {
-					console.log('Stored data:', response);
-				}
-			);
+		this.http.put(
+			environment.firebaseEndpoint + this.authService.user.value.id + '.json?auth=' + this.authService.user.value.token, 
+			{ tags, books, collections }
+		)
+		.subscribe(
+			response => {
+				console.log('Stored data:', response);
+			}
+		);
 	}
 
 	fetchData() {
-		this.http.get<{ tags: Tag[], books: Book[], collections: Collection[] }>(`${environment.firebaseEndpoint}data.json`)
-			.pipe(
-				map(data => {
-					if(!data || !data.books) { return { tags: [], books: [], collections: [] }; }
-					if(!data.tags) { data.tags = []; }
-					if(!data.collections) { data.collections = []; }
-					return {
-						tags: data.tags.map(tag => {
-							return {...tag, description: tag.description ? tag.description : null};
-						}),
-						books: data.books.map(book => {
-							return {
-								...book,
-								dateCreated: book.dateCreated ? new Date(book.dateCreated) : null,
-								dateUpdated: book.dateUpdated ? new Date(book.dateUpdated) : null,
-								tags: book.tags ? book.tags : []
-							};
-						}),
-						collections: data.collections
-					}
-				})
-			)
-			.subscribe(
-				data => {
-					console.log('Fetched data:', data);
-					this.tagService.setTags(data['tags']);
-					this.bookService.setBooks(data['books']);
-					this.collectionService.setCollections(data['collections']);
+		this.http.get<{ tags: Tag[], books: Book[], collections: Collection[] }>(
+			environment.firebaseEndpoint + this.authService.user.value.id + '.json?auth=' + this.authService.user.value.token
+		)
+		.pipe( 
+			map(data => {
+				if(!data || !data.books) { return { tags: [], books: [], collections: [] }; }
+				if(!data.tags) { data.tags = []; }
+				if(!data.collections) { data.collections = []; }
+				return {
+					tags: data.tags.map(tag => {
+						return {...tag, description: tag.description ? tag.description : null};
+					}),
+					books: data.books.map(book => {
+						return {
+							...book,
+							dateCreated: book.dateCreated ? new Date(book.dateCreated) : null,
+							dateUpdated: book.dateUpdated ? new Date(book.dateUpdated) : null,
+							tags: book.tags ? book.tags : []
+						};
+					}),
+					collections: data.collections
 				}
-			);
+			})
+		)
+		.subscribe(
+			data => {
+				console.log('Fetched data:', data);
+				this.tagService.setTags(data['tags']);
+				this.bookService.setBooks(data['books']);
+				this.collectionService.setCollections(data['collections']);
+			}
+		);
 	}
 }
--- src/main.ts
+++ src/main.ts
@@ -9,13 +9,13 @@ import { AppComponent } from './app/app.component';
 import { routes } from './app/app-routing';
 
 if (environment.production) {
-  enableProdMode();
+  	enableProdMode();
 }
 
 bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(routes),
-    provideAnimations(),
-    importProvidersFrom(HttpClientModule)
-  ],
+	providers: [
+		provideRouter(routes),
+		provideAnimations(),
+		importProvidersFrom(HttpClientModule),
+	],
 });
